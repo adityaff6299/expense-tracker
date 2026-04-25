456d38a63c3782eb7ab8bee02887dc7ba5f659c6
Aditya <adityaff6299@gmail.com>
2026-04-25 15:31:28 +0530

    Updated TEMPLATES DIRS in settings

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'expense_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'tracker' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
